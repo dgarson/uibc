8e6c33c953aea1896c963d5cf2c3ad86ea99b5e3
dgarson <dgarson@gmail.com>
2024-05-26 13:37:36 -0600

    fix: minor bugs in core

--- uibc.bat
+++ uibc.bat
@@ -1,6 +1,5 @@
-cd C:\dev\metas\AlMac\uibc
-C:\dev\metas\metaf\metaf.exe ./UltimateIBControl.af ./UltimateIBControl.met
-C:\dev\metas\metaf\metaf.exe ./UltimateIBControl_Core.af ./UltimateIBControl_Core.met
+%~dp0\metaf\metaf.exe ./UltimateIBControl.af ./UltimateIBControl.met
+%~dp0\metaf\metaf.exe ./UltimateIBControl_Core.af ./UltimateIBControl_Core.met
 
 REM xcopy /y UltimateIBControl.met C:\Games\VirindiPlugins\VirindiTank\
 xcopy /y UltimateIBControl_Core.met C:\Games\VirindiPlugins\VirindiTank\
